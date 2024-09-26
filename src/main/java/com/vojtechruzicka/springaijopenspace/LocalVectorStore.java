eb20223248e47cd9e98a93ed1cddd8ef76923f84
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2024-09-26 20:25:09 +0200

    Adding office document import

--- src/main/java/com/vojtechruzicka/springaijopenspace/LocalVectorStore.java
+++ src/main/java/com/vojtechruzicka/springaijopenspace/LocalVectorStore.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.JsonReader;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,11 +20,13 @@ import java.util.Map;
 public class LocalVectorStore extends SimpleVectorStore {
 
     private final String userHome;
+    private final Resource powerpointFile;
     private final Resource presentationsFile;
 
-    public LocalVectorStore(EmbeddingModel embeddingModel, @Value("${user.home}") String userHome, @Value("classpath:/presentations.json") Resource presentationsFile) {
+    public LocalVectorStore(EmbeddingModel embeddingModel, @Value("${user.home}") String userHome, @Value("classpath:/presentations.json") Resource presentationsFile, @Value("classpath:/2019 - SNYK.pptx") Resource powerpointFile) {
         super(embeddingModel);
         this.userHome = userHome;
+        this.powerpointFile = powerpointFile;
         this.presentationsFile = presentationsFile;
     }
 
@@ -55,11 +58,17 @@ public class LocalVectorStore extends SimpleVectorStore {
         documents.add(new Document("There are 11 different sponsors of JOpenspace CZ this year", Map.of("source","https://www.jopenspace.cz/index.html#current")));
         documents.add(new Document("42 atendees are coming to JOpenspace CZ this year.", Map.of("source","https://www.jopenspace.cz/index.html#current")));
 
+        // Example of json document import
         JsonReader jsonReader = new JsonReader(presentationsFile, _ -> Map.of("source","presentations.json","type","Jopenspace CZ presentation"));
         List<Document> jsonDocuments = jsonReader.get();
         documents.addAll(jsonDocuments);
 
-        // Then split documents to smaller more manageable chunks
+        // This reader can process office documents, PDF or HTML
+        TikaDocumentReader officeReader = new TikaDocumentReader(powerpointFile);
+        List<Document> powerpointDocuments = officeReader.read();
+        documents.addAll(powerpointDocuments);
+
+        // Split documents to smaller more manageable chunks
         TokenTextSplitter tokenSplitter = new TokenTextSplitter();
         List<Document> chunks = tokenSplitter.split(documents);
 
